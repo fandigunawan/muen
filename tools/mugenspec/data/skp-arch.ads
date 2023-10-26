0fc829ebc112ff6f4142f2e8e423fccb91b81d8c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2023-10-26 15:18:54 +0200

    Mugenspec: Add Skp.Arch template
    The new policy spec file is intended to contain architecture-specific
    data.

---
+++ tools/mugenspec/data/skp-arch.ads
@@ -0,0 +1,9 @@
+--D @Interface
+--D This package contains architecture-specific information specified by the
+--D system policy.
+package Skp.Arch
+is
+
+   Vmxon_Address : constant := 16#1000#;
+
+end Skp.Arch;
---
+++ tools/mugenspec/templates/skp-arch.ads
@@ -0,0 +1,9 @@
+--D @Interface
+--D This package contains architecture-specific information specified by the
+--D system policy.
+package Skp.Arch
+is
+
+   Vmxon_Address : constant := __vmxon_addr__;
+
+end Skp.Arch;
